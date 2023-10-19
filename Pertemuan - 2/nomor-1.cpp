38e1b18e71e369f2e062adbc588349f59500a0a1
NikeRemercier <sulthanfauzan01204@gmail.com>
2023-10-19 11:20:03 +0700

    nomor 1

---
+++ Pertemuan - 2/nomor-1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int firstValue = 10;
+    int secondValue = 8;
+    int cadangan;
+
+    cout<<"Nilai Awal Variable firstValue dan secondValue"<<endl;
+    cout<<"Nilai firstValue = "<<firstValue<<endl;
+    cout<<"Nilai secondValue = "<<secondValue<<endl;
+
+    cadangan = firstValue;
+    firstValue = secondValue;
+    secondValue = cadangan;
+
+    cout<<"\nPenukaran Variabel firstValue dan secondValue dengan Variabel Tambahan"<<endl;
+    cout<<"Nilai firstValue = "<<firstValue<<endl;
+    cout<<"Nilai secondValue = "<<secondValue<<endl;
+
+    firstValue = firstValue + secondValue;
+    secondValue = firstValue - secondValue;
+    firstValue = firstValue - secondValue;
+
+    cout<<"\nPenukaran Variabel firstValue dan secondValue tanpa Variabel Tambahan"<<endl;
+    cout<<"Nilai firstValue = "<<firstValue<<endl;
+    cout<<"Nilai secondValue = "<<secondValue<<endl;
+
+
+}
